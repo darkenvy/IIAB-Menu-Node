20937fa360fd6a67eceaad46855e48fcdb75e588
darkenvy <darkenvy6@gmail.com>
2017-03-12 15:52:33 -0700

    Changes so that development can take place live on pi with kalite & other services

--- gulpfile.js
+++ gulpfile.js
@@ -29,8 +29,8 @@ gulp.task('nodemon', function (cb) {
 gulp.task('browser-sync', ['nodemon'], function () { // for more browser-sync config options: http://www.browsersync.io/docs/options/
   browserSync.init({
     files: ['views/**/*.*', 'menu-files/**/*.*'], // watch the following files; changes will be injected (css & images) or cause browser to refresh
-    proxy: 'http://localhost:3000', // informs browser-sync to proxy our expressjs app which would run at the following location
-    port: 4000 // informs browser-sync to use the following port for the proxied app. notice that the default port is 3000, which would clash with our expressjs
+    proxy: 'http://localhost:4000', // informs browser-sync to proxy our expressjs app which would run at the following location
+    port: 5000 // informs browser-sync to use the following port for the proxied app. notice that the default port is 3000, which would clash with our expressjs
   });
 });
 
--- index.js
+++ index.js
@@ -7,7 +7,6 @@ var app = express();
 var allMenuItems = {};
 app.set('view-engine', 'bodyParser');
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/common', express.static('common'));
 app.use('/menu-files', express.static('menu-files'));
 
 // +========================================================================+ //
@@ -26,12 +25,37 @@ function init() {
       illegalChecker = illegalChecker.slice(1, illegalChecker.length) 
     }
     try { 
-      obj = JSON.parse(illegalChecker) 
-      allMenuItems[item] = obj;
+      obj = JSON.parse(illegalChecker)
+      allMenuItems[item] = createHref(obj);
     } catch (e) { 
       // Uncomment to find missing menu items
       // console.log('Illegal Characters in file: ', item, e) 
     }
+
+    function createHref(obje) {
+      switch (obje['intended_use']) {
+        case 'zim':
+          obje['href_path'] = obje.zim_name;
+          break;
+        case 'html':
+          obje['href_path'] = 'modules/' + obje.moddir;
+          break;
+        case 'webroot':
+          obje['href_path'] = '/' + obje.moddir;
+          break;
+        case 'kalite':
+          obje['href_path'] = ':8008'
+          break;
+        case 'osm':
+          obje['href_path'] = '/iiab/static/map.html'
+          break;
+        default:
+          obje['href_path'] = '#'
+          console.log('');
+      }
+      return obje;
+    }
+
   });
 
 }
@@ -43,8 +67,12 @@ function init() {
 
 app.get('/', function(req,res) {
   // console.log(Object.keys({data: allMenuItems}.data));
-  res.render('index.ejs', {data: allMenuItems});
+  // console.log(req);
+  res.render('index.ejs', {
+    data: allMenuItems,
+    domain: req.headers.host
+  });
 });
 
 init();
-app.listen(3000);
+app.listen(4000);
