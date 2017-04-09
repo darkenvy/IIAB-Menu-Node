80251b5708db25001a77ce03334e04b8dcdd98d7
Reno McKenzie <darkenvy6@gmail.com>
2017-04-08 19:56:48 -0700

    provided structure for 'finding content' feature

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var express    = require('express'),
-    fs         = require('fs'),
-    bodyParser = require('body-parser'),
-    menuItems  = require('./menuItems.module');
+fs         = require('fs'),
+bodyParser = require('body-parser'),
+xmlParse   = require('xml2js').parseString,
+    // menuItems  = require('./menuItems.module');
+    menuItems  = [];
 
-var app = express();
-var allMenuItems = {};
-app.set('view-engine', 'bodyParser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/menu-files', express.static('menu-files'));
+    var app = express();
+    var allMenuItems = {};
+    app.set('view-engine', 'bodyParser');
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use('/menu-files', express.static('menu-files'));
 
 // +========================================================================+ //
 // |                          Initialization                                | //
@@ -38,23 +40,23 @@ function init() {
     function createHref(obje) {
       switch (obje['intended_use']) {
         case 'zim':
-          obje['href_path'] = ':3000/' + obje.zim_name;
-          break;
+        obje['href_path'] = ':3000/' + obje.zim_name;
+        break;
         case 'html':
-          obje['href_path'] = '/modules/' + obje.moddir;
-          break;
+        obje['href_path'] = '/modules/' + obje.moddir;
+        break;
         case 'webroot':
-          obje['href_path'] = '/' + obje.moddir;
-          break;
+        obje['href_path'] = '/' + obje.moddir;
+        break;
         case 'kalite':
-          obje['href_path'] = ':8008/'
-          break;
+        obje['href_path'] = ':8008/'
+        break;
         case 'osm':
-          obje['href_path'] = '/iiab/static/map.html'
-          break;
+        obje['href_path'] = '/iiab/static/map.html'
+        break;
         default:
-          obje['href_path'] = '/#'
-          console.log('');
+        obje['href_path'] = '/#'
+        console.log('');
       }
       return obje;
     }
@@ -63,6 +65,48 @@ function init() {
 
 }
 
+
+// +========================================================================+ //
+// |                         Find Content Packs                             | //
+// +========================================================================+ //
+
+function createMetadata() {}
+
+function metadata() {}
+
+// Primary function that looks for content. Doesn't look for JSON metadata made by IIAB
+function getContent() {
+  // Looko for content in all places.
+  // If found, check if we have a menu-def json
+  // problem is, we may not. So a generic template will have to be made form metadata
+  // html = rachel
+  // webroot = 
+  // kalite = :8008
+  // osm = /iiab/static/map.html
+  // zim = :3000
+
+  // zims may need to be defined last as the async func is used.
+  // zim meta-data from library-xml:
+  // book id, path, indexPath, indexType, title, description, language, creator, publisher, faviconMimeType, date, articleCount, mediaCount, size
+  var modules = fs.readdirSync('/library/www/html/modules/');
+  var osm     = true;
+  var webroot = true;
+  var kalite  = true;
+  // var zims = fs.readFileSync('/library/zims/library.xml', 'utf8');
+  // xmlParse(zims, {trim: true, normalize: true, firstCharLowerCase: true}, function(err, data) {
+  //   console.log(Object.keys(data.library.book))
+  //   if (data && data.library && data.library.book) {
+  //     var zimList = data.library.book;
+  //     for (var zim in zimList) {
+  //       console.log(zimList[zim]['$'].title)
+  //     } 
+  //   }
+  // });
+
+
+
+  return ["en-afristory"];
+}
 
 // +========================================================================+ //
 // |                              Routes                                    | //
@@ -75,6 +119,7 @@ app.get('/', function(req,res) {
   });
 });
 
+menuItems = getContent();
 init();
 console.log('listening on port 4000');
 app.listen(4000);
