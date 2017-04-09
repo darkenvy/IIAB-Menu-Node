6056a3f586819df9e34418f12b39853a6a7e4b3c
Reno McKenzie <darkenvy6@gmail.com>
2017-04-09 12:20:07 -0700

    removed minifier. We didn't need it

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express        = require('express'),
     fs             = require('fs'),
-    minifyHTML     = require('express-minify-html')
     getContentList = require('./modules/content.module'),
     init           = require('./modules/initialize.module'),
     app            = express(),
@@ -15,22 +14,6 @@ var express        = require('express'),
 // +========================================================================+ //
 
 app.use('/public', express.static('views/public'));
-app.use(minifyHTML({
-  override:      true,
-  exception_url: false,
-  htmlMinifier: {
-      collapseWhitespace:           true,
-      collapseInlineTagWhitespace:  true,
-      collapseBooleanAttributes:    true,
-      removeComments:               true,
-      removeAttributeQuotes:        true,
-      removeEmptyAttributes:        true,
-      removeStyleLinkTypeAttributes:true,
-      minifyURLs:                   false,
-      minifyCSS:                    true,
-      minifyJS:                     true
-  }
-}));
 
 app.get('/', function(req,res) {
   res.render('index.ejs', {
@@ -39,8 +22,8 @@ app.get('/', function(req,res) {
   });
 });
 
-menuItems = getContentList();
-init(menuItems);
+menuItems    = getContentList();
+allMenuItems = init(menuItems);
 
 console.log(menuItems);
 console.log('listening on port 4000');
